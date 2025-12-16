1af9aea565f9c5144fc44c76f23ff30eb70be71a
Acrobatis-design <chrpanayis@gmail.com>
2025-12-16 16:03:15 +0200

    Create da.go

---
+++ code/modules/da/da.go
@@ -0,0 +1,13 @@
+package da
+
+import "harmonic-ledger/code/core"
+
+func Publish(domain core.CausalDomain) error {
+	// Publish batch data or commitments
+	// Allow reconstruction
+	return nil
+}
+
+func IsAvailable(domain core.CausalDomain) bool {
+	return true
+}
